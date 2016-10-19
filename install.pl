51f1f45e2d22a361fa4a5454686effc61abe19df
ulyfm <ulyssesfm@gmail.com>
2016-10-19 00:44:30 -0700

    tried

---
+++ install.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+# ONLY TO BE USED WHEN MAVEN CANNOT BE INSTALLED
+use strict;
+# To be updated
+system "wget https://github.com/ulyfm/FD2/releases/download/0.2b/FD2.jar";
+system "mv FD.jar FoodDrop.jar";
+
+# Need to be updated slightly less frequently
+system "mkdir FD_lib";
+system "wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/json-simple/json-simple-1.1.1.jar";
+system "mv json-simple-1.1.1.jar FD_lib/json-simple-1.1.1.jar";
+system "wget https://bitbucket.org/xerial/sqlite-jdbc/downloads/sqlite-jdbc-3.14.2.1.jar";
+system "mv sqlite-jdbc-3.14.2.1.jar FD_lib/sqlite-jdbc-3.14.2.1.jar";
