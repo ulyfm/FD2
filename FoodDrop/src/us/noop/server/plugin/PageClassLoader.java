0577912199035836a847a1cea62957bea89b1cce
Ulysses <ulyssesfm@gmail.com>
2016-10-20 13:17:27 -0700

    Update PageClassLoader.java
    fixed it a bit

--- FoodDrop/src/us/noop/server/plugin/PageClassLoader.java
+++ FoodDrop/src/us/noop/server/plugin/PageClassLoader.java
@@ -1,22 +1,18 @@
-package us.noop.ulytils;
+package us.noop.server.plugin;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
-public class PageClassLoader<T> extends ClassLoader {
+public class PageClassLoader extends ClassLoader {
 	
 	@SuppressWarnings("unchecked")
-	public T loadClass(File file, Object... args) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassFormatError, IOException{
+	public Page loadClass(File file) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassFormatError, IOException{
 		byte[] data = new byte[(int) file.length()];
 		FileInputStream f = new FileInputStream(file);
 		f.read(data);
 		f.close();
-		Class<?>[] ty = new Class<?>[args.length];
-		for(int i = 0; i < args.length; ++i){
-			ty[i] = args[i].getClass();
-		}
-		return (T) defineClass(null, data, 0, data.length).getConstructor(ty).newInstance(args);
+		return (Page) defineClass(null, data, 0, data.length).getConstructor(ty).newInstance();
 	}
 }
